9d6a56de25a7616d3911f2463aa6f51d47db4bd6
Gabriel Gonzalez <gabeg805@gmail.com>
2015-04-29 17:48:18 -0400

    Fixed navbar links, footer, and contact email back-end

--- php/form.php
+++ php/form.php
@@ -3,18 +3,19 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $from = 'From: GabeGonzalez.me';
-$to = 'gabeg@gabegonzalez.me';
+$to = 'gabe@gabegonzalez.me';
 $subject = 'Email Inquiry';
 
 $body = "From: $name\nE-Mail: $email\n\n$message";
-?>
 
-<?php
 if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) {
-        echo 'Thank you for your email!';
-    } else {
-        echo 'Oops! An error occurred. Try sending your message again.';
+    if ( mail($to, $subject, $body, $from) ) {
+        header("Location: ../home.html");
+        exit;
+    } else {        
+        echo '<p>Oops! An error occurred. Try sending your message again.</p>';
     }
+} else {
+    echo '<p>Oops! An error occurred. Try sending your message again.</p>';
 }
 ?>
